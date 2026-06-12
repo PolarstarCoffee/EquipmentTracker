3f3c42d940c4eefb059041fd3673ff89e631a152
Zuriel Zavala <zzavala2@hotmail.com>
2026-06-11 22:37:09 -0500

    Databasehelper class updates

--- Data/DatabaseHelper.cs
+++ Data/DatabaseHelper.cs
@@ -3,65 +3,141 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using System.Data.SqlClient;
-using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.SqlClient;
 
-namespace equipmentTracker.Data
+namespace DatabaseProject
 {
-    public class DatabaseHelper
+    class DatabaseHelper
     {
+        private static SqlConnection connection = new SqlConnection();
+        private static SqlCommand command = new SqlCommand();
+        private static SqlDataReader DbReader;
+        private static SqlDataAdapter adapter = new SqlDataAdapter();
+        public SqlTransaction DbTran;
 
-        public void Connect()
+        private static string strConnString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EquipmentTrackerDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Application Name=\"SQL Server Management Studio\";Command Timeout=0";
+
+
+
+        public void createConn()
         {
-            string constr;
-            //SqlConnection conn;
-            constr = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EquipmentTrackerDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Application Name=\"SQL Server Management Studio\";Command Timeout=0";
-            using (SqlConnection connection = new SqlConnection(constr))
-            {
-                try
-                {
-                    connection.Open();
-                    Console.WriteLine("Connection Opened");
-
-                    List<SqlCommand> commands = new List<SqlCommand>();
-
-                    //query commands
-                    string allQuery = "SELECT * FROM Equipment";
-                    string countQuery = "SELECT COUNT(*) FROM Equipment";
-
-                    commands.Add(new SqlCommand(allQuery, connection));
-                    commands.Add(new SqlCommand(countQuery, connection));
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error: {ex.Message}");             
-                }
-            }
-        }
-        public int TestConnection()
-        {
-            string conn;
-            // SqlConnection conn;
-            conn = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EquipmentTrackerDB;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Application Name=\"SQL Server Management Studio\";Command Timeout=0";
             try
             {
-                using (SqlConnection connection = new SqlConnection(conn))
+                if (connection.State != ConnectionState.Open)
                 {
+                    connection.ConnectionString = strConnString;
                     connection.Open();
-                    string TestQuery = "SELECT COUNT(*) FROM Equipment";
-
-                    using (SqlCommand command = new SqlCommand(TestQuery, connection))
-                    {
-                        return (int)command.ExecuteScalar();
-                
-                    }
-                    
                 }
             }
-            catch
+            catch (Exception ex)
             {
-                return -1;
+                throw ex;
+            }
+        }
+
+
+        public void closeConn()
+        {
+            connection.Close();
+        }
+
+
+        public int executeDataAdapter(DataTable tblName, string strSelectSql)
+        {
+            try
+            {
+                if (connection.State == 0)
+                {
+                    createConn();
+                }
+
+                adapter.SelectCommand.CommandText = strSelectSql;
+                adapter.SelectCommand.CommandType = CommandType.Text;
+                SqlCommandBuilder DbCommandBuilder = new SqlCommandBuilder(adapter);
+
+
+                string insert = DbCommandBuilder.GetInsertCommand().CommandText.ToString();
+                string update = DbCommandBuilder.GetUpdateCommand().CommandText.ToString();
+                string delete = DbCommandBuilder.GetDeleteCommand().CommandText.ToString();
+
+
+                return adapter.Update(tblName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public void readDatathroughAdapter(string query, DataTable tblName)
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    createConn();
+                }
+
+                command.Connection = connection;
+                command.CommandText = query;
+                command.CommandType = CommandType.Text;
+
+                adapter = new SqlDataAdapter(command);
+                adapter.Fill(tblName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public SqlDataReader readDatathroughReader(string query)
+        {
+            //DataReader used to sequentially read data from a data source
+            SqlDataReader reader;
+
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    createConn();
+                }
+
+                command.Connection = connection;
+                command.CommandText = query;
+                command.CommandType = CommandType.Text;
+
+                reader = command.ExecuteReader();
+                return reader;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public int executeQuery(SqlCommand dbCommand)
+        {
+            try
+            {
+                if (connection.State == 0)
+                {
+                    createConn();
+                }
+
+                dbCommand.Connection = connection;
+                dbCommand.CommandType = CommandType.Text;
+
+
+                return dbCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
         }
     }
